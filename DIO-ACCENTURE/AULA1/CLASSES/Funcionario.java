57b30c0b15462a9d3aeffecfb6e4821b8bbdf089
KAUA RODRIGO PINHEIRO BRAGA <kauarodrigo1193@gmail.com>
2026-01-20 21:39:18 -0300

    feat: criação de classe pessoa e funcionário e main para utilização das classes

---
+++ DIO-ACCENTURE/AULA1/CLASSES/Funcionario.java
@@ -0,0 +1,22 @@
+package CLASSES;
+
+public class Funcionario extends Pessoa {
+
+    private String cargo;
+
+    public Funcionario(String nome, int idade, String cargo) {
+        super(nome, idade);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    @Override
+    public void apresentar() {
+        System.out.println(String.format("Olá, meu nome é %s, eu tenho %d anos e trabalho como %s.", 
+            getNome(), getIdade(), cargo));
+    }
+
+}

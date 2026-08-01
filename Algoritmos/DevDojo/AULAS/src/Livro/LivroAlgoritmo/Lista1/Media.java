ad57f9ebcaca072c1c2c0fc66c661544fb574eec
KAUA RODRIGO PINHEIRO BRAGA <kauarodrigo1193@gmail.com>
2026-08-01 01:37:51 -0300

    feat: conteúdo das aulas de algoritmos que não subiram ainda

---
+++ Algoritmos/DevDojo/AULAS/src/Livro/LivroAlgoritmo/Lista1/Media.java
@@ -0,0 +1,14 @@
+package Livro.LivroAlgoritmo.Lista1;
+
+import java.util.Scanner;
+
+public class Media {
+    public static void  main(String[] args) {
+        int num1 = 8;
+        int num2 = 9;
+        int num3 = 7;
+
+        int media = (num1 + num2 + num3) / 3;
+        System.out.println(media);
+    }
+}

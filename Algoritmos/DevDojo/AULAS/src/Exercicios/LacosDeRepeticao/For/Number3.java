7bbefc03907a3f657b75bc7bb3088ad14813a198
KAUA RODRIGO PINHEIRO BRAGA <kauarodrigo1193@gmail.com>
2026-08-01 00:54:43 -0300

    feat: exercicios de laços de repetição feitos com max para aumentar um pouco da nossa lógica e melhorar gradativamente nosso conhecimento

---
+++ Algoritmos/DevDojo/AULAS/src/Exercicios/LacosDeRepeticao/For/Number3.java
@@ -0,0 +1,16 @@
+package Exercicios.LacosDeRepeticao.For;
+
+public class Number3 {
+    public static void main(String[] args) {
+        // ) Elaborar um programa que apresente o somatório dos números na faixa de 1 até 500.
+
+        int resultado = 0;
+        for(int i = 1; i <= 3; i++){
+
+            resultado = resultado + i;
+        }
+        System.out.println("O somátorio de 1 até 500 é igual a " + resultado );
+
+
+    }
+}
---
+++ Algoritmos/DevDojo/AULAS/src/Exercicios/LacosDeRepeticao/For/NumberOne.java
@@ -0,0 +1,12 @@
+package Exercicios.LacosDeRepeticao.For;
+
+public class NumberOne {
+    public static void main(String[] args) {
+        for(int i = 15 ; i<=200;i++){
+            int resultado = i * i;
+            System.out.printf("%s ao quadrado é %s %n", i, resultado);
+        }
+
+
+    }
+}
